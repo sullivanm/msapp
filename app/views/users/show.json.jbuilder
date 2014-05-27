72175a3c69bad1f7bf23ca9331275dfbc10ca4c9
fstech <fstech@fstech051u.(none)>
2014-05-27 18:27:29 +0530

    Initial Commit

---
+++ app/views/users/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @user, :id, :name, :address, :mobile, :created_at, :updated_at
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Msapp::Application.config.secret_key_base = '45b6d7b682f6ba463f2aa641c9d498602f75e8f52634fd6813faab76d59b546102cec4786ef87f4a3bf4cf3a8dbf9e9452ebe0d4c2ae88767e8aa5259e2b6709'
